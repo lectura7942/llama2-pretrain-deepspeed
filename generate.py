ef815d764194fb4025845045d4f37791103e73dc
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-12-14 11:32:25 +0900

    Fix error when generating on  cpu

--- generate.py
+++ generate.py
@@ -1,12 +1,19 @@
-from transformers import TrainingArguments, Trainer, DataCollatorForLanguageModeling
-from model import load_model
 from transformers import AutoTokenizer, LlamaForCausalLM
 import torch
 from tqdm import tqdm 
 
 MODEL_ID = "checkpoints"
-TOKENIZER_ID = "beomi/llama-2-ko-7b" # "checkpoints"
-   
+TOKENIZER_ID = "beomi/llama-2-ko-7b"
+EVAL_TEXTS = [
+        "금융정보를 받은 통신사는 인공지능 AI 을 통해",
+        "미국 증시가 "
+    ]
+
+device = torch.device(
+    "cuda:0" if torch.cuda.is_available() 
+    else "cpu"
+    )
+
 tokenizer = AutoTokenizer.from_pretrained(
     TOKENIZER_ID, 
     add_bos_token=True, 
@@ -16,27 +23,25 @@ tokenizer = AutoTokenizer.from_pretrained(
 # eod tokens added in training
 model = LlamaForCausalLM.from_pretrained(
         MODEL_ID,
-        # torch_dtype=torch.float16,
+        torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
 )
 # TODO add pad id. Currently not using
 # model.config.pad_token_id = tokenizer.pad_id
-eval_dataset = [
-        "금융정보를 받은 통신사는 인공지능 AI 을 통해",
-    ]
+
 results = []
-device = torch.device(
-    "cuda:0" if torch.cuda.is_available() 
-    else "cpu"
-    )
         
 model.to(device).eval()
     
-for idx, d in tqdm(enumerate(eval_dataset), desc="Generating", total=len(eval_dataset)):
+for idx, d in tqdm(enumerate(EVAL_TEXTS), desc="Generating", total=len(EVAL_TEXTS)):
     prompt = d
     model_input = tokenizer(prompt, return_token_type_ids=False,return_tensors="pt").to(device)
     with torch.no_grad():
-        output_text = tokenizer.decode(model.generate(**model_input, max_new_tokens=100)[0], )
+        output_text = tokenizer.decode(
+            model.generate(**model_input, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)[0], 
+            skip_special_tokens=True,
+            )
     results.append(output_text)
     
-for r in results: 
+for idx, r in enumerate(results): 
+    print(f"--- {idx} ---")
     print(r)
